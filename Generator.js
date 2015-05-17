e8178b8adb872e9c7be3260f47282cbf20214650
JoeTheAverage <billingsleyjoseph@gmail.com>
2015-05-17 18:10:52 +0100

    Restructured generator
    Refactored the generator class so that it is easier to determine what
    will be accessible when it is required.

--- Generator.js
+++ Generator.js
@@ -1,36 +1,38 @@
-function generate() {	
-	var getRandomInRange = function (min, max) {	
-		return Math.floor(Math.random() * ((max - min) + 1) + min)
-	};
-	
-	var getRandomSubset = function(array, length) {
-		var buffer = [];
-		
-		for(var i = 0; i < length; i++) {
-			var s = getRandomInRange(0, array.length - 1);
-			var r = array.splice(s, 1)[0];
-			
-			buffer.push(r);
-		}
-		
-		return buffer;
-	};
-	
-	// Small numbers are all numbers from 1 - 10 twice
-	var smallNumbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
-	var largeNumbers = [25, 50, 75, 100];
-	
-	// You can choose 0 to 4 of the large numbers
-	var largeNumberQuota = getRandomInRange(0, 4);
-	
-	var selection = 
-	{
-		target : getRandomInRange(101, 999),
-		smallNumbers : getRandomSubset(smallNumbers, 6 - largeNumberQuota),
-		largeNumbers : getRandomSubset(largeNumbers, largeNumberQuota),
-	};
-	
-	return selection;
+function getRandomInRange(min, max) {	
+	return Math.floor(Math.random() * ((max - min) + 1) + min)
 }
 
-module.exports.generate = generate;
+function getRandomSubset(array, length) {
+	var buffer = [];
+		
+	for(var i = 0; i < length; i++) {
+		var s = getRandomInRange(0, array.length - 1);
+		var r = array.splice(s, 1)[0];
+		
+		buffer.push(r);
+	}
+	
+	return buffer;
+}
+
+module.exports = {
+	generate : function() {		
+		// Small numbers are all numbers from 1 - 10 twice
+		var smallNumbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
+		var largeNumbers = [25, 50, 75, 100];
+		
+		// You can choose 0 to 4 of the large numbers
+		var largeNumberQuota = getRandomInRange(0, 4);
+		
+		var largeNumbers = getRandomSubset(largeNumbers, largeNumberQuota);
+		var smallNumbers = getRandomSubset(smallNumbers, 6 - largeNumberQuota);
+		
+		var selection = 
+		{
+			target : getRandomInRange(101, 999),
+			numbers : largeNumbers.concat(smallNumbers)
+		};
+		
+		return selection;
+	}
+}
