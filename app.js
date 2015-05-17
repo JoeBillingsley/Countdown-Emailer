d7cc7934a3fcc24f547ef878f5867e0d3e8c76d2
JoeTheAverage <billingsleyjoseph@gmail.com>
2015-05-17 18:16:21 +0100

    Initial commit
    The intended entry point of the program. Generates a puzzle and emails
    it to all intended recipient.

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var generator = require('./Generator.js');
-var solver = require('./Solver.js');
+// Generate the puzzle
+var puzzleGen = require('./Generator.js');
+var puzzle = puzzleGen.generate();
 
-console.log(solver.solve(generator.generate()));
+var subject = "Your daily countdown puzzle!";
+var body = "Morning all! \r\n\n" + 
+		   "Today you must try and reach " + puzzle.target + " with the numbers " + puzzle.numbers + ". \r\n\n" +
+		   "Best of luck!";
+
+// Send the email
+var Mailer = require('./Emailer.js');
+var mailer = new Mailer("username", "password");
+
+mailer.sendMessage(subject, body);
