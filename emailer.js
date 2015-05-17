aaba664d6bd89e66f99e24b6f5029ddc8947ae9d
JoeTheAverage <billingsleyjoseph@gmail.com>
2015-05-17 18:13:48 +0100

    Restructured to be a type rather than singleton

--- emailer.js
+++ emailer.js
@@ -1,27 +1,33 @@
-var mailer = require('nodemailer');
+module.exports = Emailer;
 
-var sender = 'countdown@josephbillingsley.co.uk';
-var receivers = 'user@anywhere.co.uk, otheruser@somewhere.com';
+function Emailer(sender, password) {
+	var mailer = require('nodemailer');
+	
+	this._sender = sender;
+	this._transport = mailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: sender,
+			pass: password,
+		}
+	});
+}
 
-var transport = mailer.createTransport({
-	service: 'Gmail',
-	auth: {
-		user: sender,
-		pass: '*****'
-	}
-});
-
-var mailOptions = {
-	from: 'Countdown Mailer <' + sender + '>',
-	to: receivers,
-	subject: 'Test email',
-	text: '1 2 3 4 5'
-};
-
-transport.sendMail(mailOptions, function(error, info) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('Message sent: ' + info.response);
-	}
-});
+Emailer.prototype.sendMessage = function (subject, text) {
+	var _receivers = require('./ReceiversRepo.js').getAll();
+	
+	var mailOptions = {
+		from: 'Countdown Mailer <' + this._sender + '>',
+		to: _receivers,
+		subject: subject,
+		text: text
+	};
+		
+	this._transport.sendMail(mailOptions, function(error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+				console.log('Message sent: ' + info.response);
+		}
+	});
+}
