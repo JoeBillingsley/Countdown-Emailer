6322cfc52113c88b0eb72e8b780538c3f05d456f
JoeTheAverage <billingsleyjoseph@gmail.com>
2015-05-29 13:18:16 +0100

    Added shebang for running in node.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 // Generate the puzzle
 var puzzleGen = require('./Generator.js');
 var puzzle = puzzleGen.generate();
@@ -11,7 +13,7 @@ var receivers = require('./ReceiversRepo.js').getAll().receivers;
 
 // Send the email
 var Mailer = require('./Emailer.js');
-var mailer = new Mailer("username", "password");
+var mailer = new Mailer("billingsleyjoseph@gmail.com", "maximillion93");
 
 receivers.forEach(function(receiver) {
 	mailer.sendMessage(subject, body, receiver);
